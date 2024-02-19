f6ee88a9613f4062a4d5f1e125fbb271124efb41
BENABDELLAH Achraf <achraf.benabdellah@grenoble-inp.org>
2024-02-19 14:45:43 +0100

    FEAT : website init

---
+++ src/fullscreen.js
@@ -0,0 +1,12 @@
+function toggleFullscreen() {
+    var elem = document.querySelector('.game-window');
+    if (!document.fullscreenElement) {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+}

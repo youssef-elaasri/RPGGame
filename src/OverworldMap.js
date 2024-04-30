6ebecaa68faa9083c4936c71da06d03eb4d09950
elaasriy <youssef.elaasri@grenoble-inp.org>
2024-04-30 12:32:16 +0200

    making cpp's chalenges about basics python chalenges

--- src/OverworldMap.js
+++ src/OverworldMap.js
@@ -92,97 +92,34 @@ window.OverworldMaps = {
         gameObjects: {
         },
         NPCs: {
-            /* ElProfesor: new NPC({
-                name : "El Profesor",
-                dialogues : ["Hello!", "Welcome to INP Legends."],
-                x: util.inGrid(9),
-                y:util.inGrid(3),
-                behaviorLoop: [
-                    { type: "walk", direction: "right"},
-                    { type: "walk", direction: "right"},
-                    { type: "stand", direction:"right", time: 1000},
-                    { type: "walk", direction:"right"},
-                    { type: "walk", direction: "right"},
-                    { type: "stand", direction:"right", time: 1000},
-                    { type: "stand", direction:"left", time: 2000},
-                    { type: "walk", direction:"left"},
-                    { type: "walk", direction:"left"},
-                    { type: "stand", direction:"left", time: 800},
-                    { type: "walk", direction:"left"},
-                    { type: "walk", direction:"left"},
-                    { type: "stand", direction:"left", time: 800},
-                    { type: "stand", direction:"right", time: 3000},
-
-
-                ]
-            }), */
-            securityChief: new NPC({
-                name : "securityChief",
-                src : "src/images/NPCS/securityChief.png",
+            adventurer : new NPC({
+                name : "adventurer",
+                src : "src/images/NPCS/adventurer.png",
                 dialogues : {
-                    "chiffrement_cesar" : ["Impressive work!", 
-                                            "You've resecured our systems and learned a valuable skill in the process. Your ability to handle such a crisis is commendable.",
-                                            "As a token of our gratitude, I will tell you a secret to know more about problem solving",
-                                            "you see the class at the right ?",
-                                            "there you can find a Facility Manager who you can help solving some problem and gain more knowledge"],
+                    "convert_to_float" : [
+                        "Analysis of the output initiated...", 
+                        "Processing complete.",
+                        "The function has successfully restored data integrity for our experimental results.",
+                        "You have neutralized one of the AI’s disruptive tactics.",
+                        "With the data corrected, I recommend deploying diagnostic scripts to identify further system anomalies.",
+                        "Your proficiency with algorithms will be paramount.",
+                    ]
                 },
-                defaultDialogue : ["Thank goodness you're here!",
-                "We've suffered a major security breach. Our access systems have been compromised, and sensitive data is at risk.",
-                "We need to secure our communications using the Caesar cipher.",
-                "It's simple but effective. You need to write a Python function that can encrypt messages given to you by our department heads.",
-                "It shifts each letter in the message by a predetermined number down the alphabet.",
-                "For example, with a shift of 3, 'A' becomes 'D'.",
-                "You'll need to make sure the shift wraps around the end of the alphabet and keeps the letter casing the same."],
+                defaultDialogue : [
+                    "Greetings, User! My programming has detected anomalies in data processing modules due to sabotage by the rogue AI.",
+                    "I require human intelligence to correct these errors.",
+                    "The AI has jumbled the numerical outputs of our experiments, intermixing valid numerical strings with gibberish.", 
+                    "We need to filter and convert these readings to utilize them in restoring system functionalities.",
+                    "In cases of failed conversion, replace it with None.",
+                    "This will allow my systems to identify and disregard corrupt data while maintaining array integrity for further analysis.",
+                    "Please proceed with the following template. Adapt it as necessary to ensure robust data cleansing."
+                ],
                 x: util.inGrid(15),
                 y:util.inGrid(10),
                 challenge : () => util.runChallenge({
-                    fileName : "chiffrement_cesar",
-                    NPCname: "securityChief"
+                    fileName : "convert_to_float",
+                    NPCname: "adventurer"
                 })
-            }),
-            facilityManager: new NPC({
-                name : "facilityManager",
-                src : "src/images/NPCS/facilityManager.png",
-                dialogues : {
-                    "tri_batiments" : ["Excellent work! Now, let's allocate the energy accordingly and bring back power where it's needed most.",
-                                        "to do that you must talk with Achraf, you will find him in the left classroom.",
-                                        "he is a bit stupid, so just try to be nice with him"
-                ],
-                    "chiffrement_cesar" : ["We're in a dire situation. ",
-                    "Our main power grid has been sabotaged, causing blackouts in crucial buildings.",
-                    "We need your expertise to manage this crisis efficiently.",
-                    "We have a limited amount of energy available, and multiple buildings need power." ,
-                    "Each building has a specific energy need and a level of importance.",
-                    "Your task is to allocate our scarce resources to maximize the overall importance of the buildings that get powered.",
-                    "Start by writing a function that sorts the buildings.",
-                    "We need them ordered by the ratio of their importance to energy consumption. The higher the ratio, the higher the priority."
-                ],
-                },
-                defaultDialogue : [
-                    "We're in a dire situation. ",
-                     "Our main power grid has been sabotaged, causing blackouts in crucial buildings.",
-                    "but never mind, I think there is a lot of people here who need your help more than I do"
-                ],
-                x: util.inGrid(45),
-                y:util.inGrid(23),
-                challenge: () => util.runChallenge({
-                    fileName : "tri_batiments",
-                    NPCname : "facilityManager",
-                    flags : ["chiffrement_cesar"]
-                }),
-            }),
-            achraf : new NPC ({
-                name : "achraf",
-                src : "src/images/NPCS/achraf.png",
-                dialogues : {
-                    
-                },
-                defaultDialogue : [
-                    "..."
-                ],
-                x: util.inGrid(10),
-                y:util.inGrid(23),
-
             })
         },
         walls : {
--- story_line/CPP/defi2.py
+++
@@ -1,4 +0,0 @@
-
-
-def tri_batiments(batiments):
-    return sorted(batiments, key = lambda batiment: batiment[1]/batiment[0], reverse= True)
